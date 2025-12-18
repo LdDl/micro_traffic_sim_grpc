9cfaae9ec3eaa3e08d6eeb364a5007c76ba5af61
Dimitrii Lopanov <sexykdi@gmail.com>
2025-12-18 20:42:31 +0300

    change build

--- docker_registry.sh
+++
@@ -1,3 +0,0 @@
-# Script for pushing to Docker Registry
-make docker-build REG=@todo IMAGE=micro-traffic-sim/server TAG=latest`
-make docker-push REG=@todo IMAGE=micro-traffic-sim/server TAG=latest`
